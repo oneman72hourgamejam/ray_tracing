f7f28bcf42fe9527f51272a04550836ba69c70b1
oneman72hourgamejam <oneman72hourgamejam@gmail.com>
2019-03-18 23:08:46 +0300

    added camera class

--- source/main.cpp
+++ source/main.cpp
@@ -6,13 +6,35 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include <cfloat>
+#include "camera.h"
+#define _XOPEN_SOURCE
+#include <stdlib.h>
+
+double drand48(void);
+
+double drand48(void)
+{
+	return (rand() / (RAND_MAX + 1.0));
+}
+
+vec3 random_in_unit_shpere()
+{
+	vec3 p;
+	do
+	{
+		p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+	} 
+	while (p.squared_lenght() >= 1.0);
+	return p;
+}
 
 vec3 color(const ray& r, hitable *world)
 {
 	hit_record rec;
-	if (world->hit(r, 0.0, FLT_MAX, rec))
+	if (world->hit(r, 0.001, FLT_MAX, rec))
 	{
-		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+		vec3 target = rec.p + rec.normal + random_in_unit_shpere();
+		return 0.5 * color(ray(rec.p, target - rec.p), world);
 	}
 	else
 	{
@@ -29,30 +51,36 @@ int main()
 
 	int nx = 200;
 	int ny = 100;
+	int ns = 100;
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 	myfile << "P3\n" << nx << " " << ny << "\n255\n";
-
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
 
 	hitable *list[2];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
 	hitable *world = new hitable_list(list, 2);
+	camera cam;
 
 	for (int j = ny-1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
 			//vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
+			vec3 col(0, 0, 0);
+			for (int s = 0; s < ns; s++)
+			{
+				float u = float(i + drand48()) / float(nx);
+				float v = float(j + drand48()) / float(ny);
 
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 col = color(r, world);
+				ray r = cam.get_ray(u, v);
+				//ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+				vec3 p = r.point_at_parameter(2.0);
+				//vec3 col = color(r, world);
+				col += color(r, world);
+			}
+			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
