2fc06aab67bb2c749c6557f1ce16fa9fb238be79
oneman72hourgamejam <oneman72hourgamejam@gmail.com>
2019-03-18 21:24:29 +0300

    added vec3 class

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <fstream>
+#include "vec3.h"
 
 int main()
 {
@@ -19,16 +20,17 @@ int main()
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
+
 			std::cout << ir << " " << ig << " " << ib << "\n";
 			myfile << ir << " " << ig << " " << ib << "\n";
 		}
 	}
+
 	myfile.close();
 
 	return 0;
